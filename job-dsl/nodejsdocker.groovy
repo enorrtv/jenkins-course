73c10daad538da4d6db1c1096e73ad7e7ccd4f99
enorrtv <44398810+enorrtv@users.noreply.github.com>
2020-05-16 19:39:22 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('enorrtv/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('enorrtv')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
